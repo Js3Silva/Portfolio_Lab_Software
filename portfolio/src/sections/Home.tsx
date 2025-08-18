6d779cb4f8a5be7ca726c71869b1e45b2e62b49a
Jonathan Sena da Silva <jonathansilvagw@gmail.com>
2025-08-18 20:49:50 -0300

    Página inicial concluída

--- portfolio/src/sections/Home.tsx
+++ portfolio/src/sections/Home.tsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import "../assets/CSS/home.css";
+import img from "../assets/astronauta.png";
 
 export default function Home() {
   const paragraphText = `Intro text: Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.`;
@@ -8,13 +9,13 @@ export default function Home() {
 
   useEffect(() => {
     const words = paragraphText.split(" ");
-    const containerWidth = 60; // ajusta baseado no seu max-width
+    const containerWidth = 60;
     const tempLines: string[][] = [];
     let currentLine: string[] = [];
     let currentLength = 0;
 
     words.forEach((word) => {
-      currentLength += word.length + 1; // +1 pelo espaço
+      currentLength += word.length + 1;
       if (currentLength > containerWidth) {
         tempLines.push(currentLine);
         currentLine = [word];
@@ -29,23 +30,32 @@ export default function Home() {
 
   return (
     <div className="home">
-      <h1 className="home-h1">{Name}</h1>
-      <section className="home-description">
-        <p>
-          {lines.map((line, index) => (
-            <span
-              key={index}
-              className={index === lines.length - 1 ? "line last-line" : "line"}
-              style={{ "--i": index } as React.CSSProperties}
-            >
-              {line.join(" ")}{index === lines.length - 1 ? " " : ""}
-            </span>
-          ))}
-        </p>
-      </section>
-      <section className="home-button-section">
-        <button className="home-button">Continuar</button>
-      </section>
+      <div className="home-content">
+        {/* Texto */}
+        <div className="home-text">
+          <h1 className="home-h1">{Name}</h1>
+          <section className="home-description">
+            <p>
+              {lines.map((line, index) => (
+                <span
+                  key={index}
+                  className={index === lines.length - 1 ? "line last-line" : "line"}
+                  style={{ "--i": index } as React.CSSProperties}
+                >
+                  {line.join(" ")}{index === lines.length - 1 ? " " : ""}
+                </span>
+              ))}
+            </p>
+          </section>
+          <section className="home-button-section">
+            <button className="home-button">Continuar</button>
+          </section>
+        </div>
+
+        <div className="home-image">
+          <img src={img} alt="Foto decorativa de um Astronauta" />
+        </div>
+      </div>
     </div>
   );
 }
